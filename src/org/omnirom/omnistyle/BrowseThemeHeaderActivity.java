9c536518b63b4c5ceaea559ee2b577884e1907e1
maxwen <max.weninger@gmail.com>
2017-08-20 02:09:55 +0200

    OmniStyle: add theme headers and friends
    add a new activity to browser only the theme headers
    add a new activity to pick static header image from settings
    change layout of browse activities to use header style
    
    Change-Id: Ifc286385575592c9d3879e230e72ba1152324680

--- src/org/omnirom/omnistyle/BrowseThemeHeaderActivity.java
+++ src/org/omnirom/omnistyle/BrowseThemeHeaderActivity.java
@@ -22,8 +22,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -41,16 +41,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.omnirom.omnistyle.HeaderUtils.DaylightHeaderInfo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class BrowseHeaderActivity extends Activity {
-    private static final String TAG = "BrowseHeaderActivity";
+public class BrowseThemeHeaderActivity extends Activity {
+    private static final String TAG = "BrowseThemeHeaderActivity";
     private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
     private static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
     private static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
@@ -61,15 +58,11 @@ public class BrowseHeaderActivity extends Activity {
     private String mPackageName;
     private String mHeaderName;
     private ListView mHeaderListView;
-    private Spinner mHeaderSelect;
-    private Map<String, String> mHeaderMap;
-    private List<String> mLabelList;
     private HeaderListAdapter mHeaderListAdapter;
     private ProgressBar mProgress;
     private TextView mCreatorName;
     private String mCreatorLabel;
     private String mCreator;
-    protected boolean mPickerMode;
 
     public class HeaderListAdapter extends ArrayAdapter<DaylightHeaderInfo> {
         private final LayoutInflater mInflater;
@@ -125,49 +118,29 @@ public class BrowseHeaderActivity extends Activity {
         getActionBar().setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
 
         mProgress = (ProgressBar) findViewById(R.id.browse_progress);
-        mHeaderSelect = (Spinner) findViewById(R.id.package_select);
         mCreatorName = (TextView) findViewById(R.id.meta_creator);
         mCreatorLabel = getResources().getString(R.string.header_creator_label);
-        mHeaderMap = new HashMap<String, String>();
-        mLabelList = new ArrayList<String>();
-        getAvailableHeaderPacks(mHeaderMap);
+        Spinner s = (Spinner) findViewById(R.id.package_select);
+        s.setVisibility(View.GONE);
 
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this,
-                R.layout.spinner_item, mLabelList);
-        mHeaderSelect.setAdapter(adapter);
-
-        mHeaderSelect.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String label = mLabelList.get(position);
-                loadHeaderPackage(mHeaderMap.get(label));
-                mCreatorName.setVisibility(TextUtils.isEmpty(mCreator) ? View.GONE : View.VISIBLE);
-                mCreatorName.setText(mCreatorLabel + mCreator);
-                mHeaderListAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
-        });
 
         mHeadersList = new ArrayList<DaylightHeaderInfo>();
         mHeaderListView = (ListView) findViewById(R.id.package_images);
-        if (mPickerMode) {
-            mHeaderListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    DaylightHeaderInfo di = mHeadersList.get(i);
-                    Settings.System.putString(getContentResolver(), STATUS_BAR_CUSTOM_HEADER_IMAGE, mPackageName + "/" + di.mImage);
-                    Settings.System.putString(getContentResolver(), STATUS_BAR_CUSTOM_HEADER_PROVIDER, "static");
-                    Toast.makeText(BrowseHeaderActivity.this, R.string.custom_header_image_notice, Toast.LENGTH_LONG).show();
-                }
-            });
-        }
+        mHeaderListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DaylightHeaderInfo di = mHeadersList.get(i);
+                Settings.System.putString(getContentResolver(), STATUS_BAR_CUSTOM_HEADER_IMAGE, mPackageName + "/" + di.mImage);
+                Settings.System.putString(getContentResolver(), STATUS_BAR_CUSTOM_HEADER_PROVIDER, "static");
+                Toast.makeText(BrowseThemeHeaderActivity.this, R.string.custom_header_image_notice, Toast.LENGTH_LONG).show();
+            }
+        });
         mHeaderListAdapter = new HeaderListAdapter(this);
         mHeaderListView.setAdapter(mHeaderListAdapter);
-        loadHeaderPackage(mHeaderMap.get(mLabelList.get(0)));
+        loadThemeHeaderPackage();
         mHeaderListAdapter.notifyDataSetChanged();
+        mCreatorName.setVisibility(TextUtils.isEmpty(mCreator) ? View.GONE : View.VISIBLE);
+        mCreatorName.setText(mCreatorLabel + mCreator);
     }
 
     @Override
@@ -178,52 +151,12 @@ public class BrowseHeaderActivity extends Activity {
         return true;
     }
 
-    private void getAvailableHeaderPacks(Map<String, String> headerMap) {
-        String defaultLabel = null;
-        Intent i = new Intent();
-        PackageManager packageManager = getPackageManager();
-        i.setAction("org.omnirom.DaylightHeaderPack");
-        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-            String packageName = r.activityInfo.packageName;
-            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
-            if (label == null) {
-                label = r.activityInfo.packageName;
-            }
-            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
-                defaultLabel = label;
-            } else {
-                headerMap.put(label, packageName);
-            }
-        }
-        i.setAction("org.omnirom.DaylightHeaderPack1");
-        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-            String packageName = r.activityInfo.packageName;
-            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
-            if (label == null) {
-                label = packageName;
-            }
-            headerMap.put(label, packageName + "/" + r.activityInfo.name);
-        }
-        mLabelList.addAll(headerMap.keySet());
-        Collections.sort(mLabelList);
-        if (defaultLabel != null) {
-            mLabelList.add(0, defaultLabel);
-            headerMap.put(defaultLabel, DEFAULT_HEADER_PACKAGE);
-        }
-    }
-
-    private void loadHeaderPackage(String label) {
-        if (DEBUG) Log.i(TAG, "Load header pack " + label);
+    private void loadThemeHeaderPackage() {
+        if (DEBUG) Log.i(TAG, "Load header pack ");
         mProgress.setVisibility(View.VISIBLE);
-        int idx = label.indexOf("/");
-        if (idx != -1) {
-            String[] parts = label.split("/");
-            mPackageName = parts[0];
-            mHeaderName = parts[1];
-        } else {
-            mPackageName = label;
-            mHeaderName = null;
-        }
+        mPackageName = "org.omnirom.omnistyle";
+        mHeaderName = "org.omnirom.omnistyle.theme";
+
         try {
             PackageManager packageManager = getPackageManager();
             mRes = packageManager.getResourcesForApplication(mPackageName);
---
+++ src/org/omnirom/omnistyle/PickHeaderActivity.java
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.omnirom.omnistyle;
+
+import android.os.Bundle;
+
+public class PickHeaderActivity extends BrowseHeaderActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mPickerMode = true;
+        super.onCreate(savedInstanceState);
+    }
+}
