f18c62cf5f16e434f04bf8a7193c7738bbee8154
maxwen <max.weninger@gmail.com>
2017-08-23 19:23:17 +0200

    OmniStyle: fix build for android-8.0
    and for now add a launcher activity for wallpaper browser
    
    Change-Id: Ia92554b9dfe7f3a8a66e0321e1f32cc0fcbe6914

--- Android.mk
+++ Android.mk
@@ -24,7 +24,8 @@ else
 support_library_root_dir := prebuilts/sdk/current/support
 endif
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview \
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4 \
+    android-support-v7-cardview \
     android-support-v7-recyclerview \
     android-support-v7-gridlayout \
     android-support-core-ui \
